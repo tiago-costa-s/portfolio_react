2395efea824b16551c930baf6789f89dad6f0842
tiago-costa-s <tiagocostasantos0013@gmail.com>
2024-02-14 13:20:45 -0300

    Configurando NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
+import NavBar from './navbar/NavBar';
 
 function App() {
 
   return (
 
     <div className='App'>
-
+      <NavBar />
     </div>
 
   )
--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,10 +1,32 @@
-
+// style
 import './NavBar.css'
+
+
+// 
+import { Link as ScrollLink } from 'react-scroll'
 
 const NavBar = () => {
 
     return (
-        <div className="nav-bar">NavBar</div>
+        <nav className='navbar'>
+            <ul className='navbar-nav'>
+                <li className='nav-item'>
+                    <ScrollLink to='/home'>Home</ScrollLink>
+                </li>
+                <li className='nav-item'>
+                    <ScrollLink to='/aboutme'>sobre min</ScrollLink>
+                </li>
+                <li className='nav-item'>
+                    <ScrollLink to='/stack'>tech stack</ScrollLink>
+                </li>
+                <li className='nav-item'>
+                    <ScrollLink to='/project'>projeto</ScrollLink>
+                </li>
+                <li className='nav-item'>
+                    <ScrollLink to='/contact'>contato</ScrollLink>
+                </li>
+            </ul>
+        </nav>
     )
 
 }
